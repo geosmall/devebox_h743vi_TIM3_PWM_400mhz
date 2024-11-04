b948e7ecb5f2f2007aa0d4c029da7159ba0b0e65
George Small <geomall92@gmail.com>
2024-11-04 06:44:35 -0500

    Modified to TIM3

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -30,24 +30,24 @@
 /* User can use this section to tailor TIMx instance used and associated
    resources */
 /* Definition for TIMx clock resources */
-#define TIMx                           TIM1
+#define TIMx                           TIM3
 
-#define TIMx_CLK_ENABLE()              __HAL_RCC_TIM1_CLK_ENABLE()
+#define TIMx_CLK_ENABLE()              __HAL_RCC_TIM3_CLK_ENABLE()
 
 /* Definition for TIMx Channel Pins */
-#define TIMx_CHANNEL_GPIO_PORT()       __HAL_RCC_GPIOA_CLK_ENABLE();
-#define TIMx_GPIO_PORT_CHANNEL1        GPIOA
-#define TIMx_GPIO_PORT_CHANNEL2        GPIOA
-#define TIMx_GPIO_PORT_CHANNEL3        GPIOA
-#define TIMx_GPIO_PORT_CHANNEL4        GPIOA
-#define TIMx_GPIO_PIN_CHANNEL1         GPIO_PIN_8
-#define TIMx_GPIO_PIN_CHANNEL2         GPIO_PIN_9
-#define TIMx_GPIO_PIN_CHANNEL3         GPIO_PIN_10
-#define TIMx_GPIO_PIN_CHANNEL4         GPIO_PIN_11
-#define TIMx_GPIO_AF_CHANNEL1          GPIO_AF1_TIM1
-#define TIMx_GPIO_AF_CHANNEL2          GPIO_AF1_TIM1
-#define TIMx_GPIO_AF_CHANNEL3          GPIO_AF1_TIM1
-#define TIMx_GPIO_AF_CHANNEL4          GPIO_AF1_TIM1
+#define TIMx_CHANNEL_GPIO_PORT()       __HAL_RCC_GPIOB_CLK_ENABLE();
+#define TIMx_GPIO_PORT_CHANNEL1        GPIOB
+#define TIMx_GPIO_PORT_CHANNEL2        GPIOB
+#define TIMx_GPIO_PORT_CHANNEL3        GPIOB
+#define TIMx_GPIO_PORT_CHANNEL4        GPIOB
+#define TIMx_GPIO_PIN_CHANNEL1         GPIO_PIN_4
+#define TIMx_GPIO_PIN_CHANNEL2         GPIO_PIN_5
+#define TIMx_GPIO_PIN_CHANNEL3         GPIO_PIN_0
+#define TIMx_GPIO_PIN_CHANNEL4         GPIO_PIN_1
+#define TIMx_GPIO_AF_CHANNEL1          GPIO_AF2_TIM3
+#define TIMx_GPIO_AF_CHANNEL2          GPIO_AF2_TIM3
+#define TIMx_GPIO_AF_CHANNEL3          GPIO_AF2_TIM3
+#define TIMx_GPIO_AF_CHANNEL4          GPIO_AF2_TIM3
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
 
